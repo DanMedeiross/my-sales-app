5514043f864e98d46de994b6755e26567d6762f5
Daniel Medeiros <danmedeirosws@gmail.com>
2024-12-11 15:24:37 -0300

    The beginning
    Components, schematics, navigation, home, categories, routes and dashboard.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatSlideToggleModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-sales-app';
-}
+export class AppComponent { }
